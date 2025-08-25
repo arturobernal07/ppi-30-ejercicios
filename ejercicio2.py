fe7fc9a21cbffc944a1699d98fbad5ba78d3dccb
arturin <bernalartur363@gmail.com>
2025-08-24 20:45:30 -0600

    Modificaciones en ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,11 +1,11 @@
-class libro:
+class Libro:
     def __init__(self, titulo, autor, precio):
         self.titulo = titulo
         self.autor = autor
         self.precio = precio
 
     def mostrar_informaciones(self):
-        print(f"El libro: '{self.titulo}', escrito por: '{self.autor}', se vende a {self.precio} euros.")
+        print(f"El libro titulado '{self.titulo}', escrito por la autora '{self.autor}', se vende a {self.precio} euros.")
 
-
-libro_1 = libro("Donde te has ido mujer", "P. Elegante", 16)
+libro_1 = Libro("100 Ejercicios Python para practicar", "Laurentine K.Masson", 9.99)
+libro_1.mostrar_informaciones()
